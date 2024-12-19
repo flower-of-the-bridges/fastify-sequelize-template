e698bf91a169d65c542e44a3252e867d95e33bc4
flower-of-the-bridges <gfiordeponti@gmail.com>
2024-12-19 09:41:42 +0100

    chore: tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,16 @@
 import { describe, it } from 'node:test'
 import { Application } from '../src/infrastructure/application'
 import assert from 'node:assert'
+import { configSchema } from '../src/infrastructure/application/schemas'
 
 describe('[app] startup', () => {
 
   it('should start with needed env variables', async() => {
-    process.env.API_KEY = 'some-api-key'
     const application = new Application()
     await application.start()
+
+    assert.strictEqual(application['app'].config.HTTP_PORT, configSchema.properties.HTTP_PORT.default, 'HTTP_PORT should have default value')
+    assert.strictEqual(application['app'].config.DB_PATH, configSchema.properties.DB_PATH.default, 'DB_PATH should have default value')
 
     await application['stop']()
   })
